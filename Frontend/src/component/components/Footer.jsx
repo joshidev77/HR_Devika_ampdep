0ceb2c6fd7bab5b6788968b76f3249d5189a23ee
Bhuvnesh Sanathara <68009290+bhuvisanathra@users.noreply.github.com>
2024-07-26 17:19:20 +0530

    Footer and navbar added!

---
+++ Frontend/src/component/components/Footer.jsx
@@ -0,0 +1,160 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <section className="py-10 bg-gray-50 sm:pt-16 lg:pt-24">
+      {/* <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-y-16 gap-x-12">
+          <div className="col-span-2 md:col-span-3 lg:col-span-2 lg:pr-8">
+            <div className="flex-shrink-0">
+              <a href="/homepage" title="" className="footer-heading">
+                Language Learner
+              </a>
+            </div>
+
+            <p className="text-base leading-relaxed text-gray-600 mt-7">
+              Learn any language easily
+            </p>
+          </div>
+
+          <div className="company">
+            <p className="text-sm font-semibold tracking-widest text-gray-400 uppercase">
+              Company
+            </p>
+
+            <ul className="mt-6">
+              <li>
+                <a
+                  href="/AboutUs"
+                  title=""
+                  className="flex text-base text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600"
+                >
+                  {" "}
+                  About{" "}
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="#"
+                  title=""
+                  className="flex text-base text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600"
+                >
+                  {" "}
+                  Features{" "}
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="#"
+                  title=""
+                  className="flex text-base text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600"
+                >
+                  {" "}
+                  Works{" "}
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="#"
+                  title=""
+                  className="flex text-base text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600"
+                >
+                  {" "}
+                  Career{" "}
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="help">
+            <p className="text-sm font-semibold tracking-widest text-gray-400 uppercase">
+              Help
+            </p>
+
+            <ul className="mt-6">
+              <li>
+                <a
+                  href="#"
+                  title=""
+                  className="flex text-base text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600"
+                >
+                  {" "}
+                  Customer Support{" "}
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="#"
+                  title=""
+                  className="flex text-base text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600"
+                >
+                  {" "}
+                  Languages{" "}
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="#"
+                  title=""
+                  className="flex text-base text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600"
+                >
+                  {" "}
+                  Terms & Conditions{" "}
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="#"
+                  title=""
+                  className="flex text-base text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600"
+                >
+                  {" "}
+                  Privacy Policy{" "}
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="col-span-2 md:col-span-1 lg:col-span-2 lg:pl-8">
+            <p className="text-sm font-semibold tracking-widest text-gray-400 uppercase">
+              Subscribe to newsletter
+            </p>
+
+            <form action="#" method="POST" className="mt-6">
+              <div>
+                <label htmlFor="email" className="sr-only">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="Enter your email"
+                  className="emailbox"
+                />
+              </div>
+
+              <button type="submit" className="btn btn-green">
+                Subscribe
+              </button>
+            </form>
+          </div>
+        </div>
+      </div> */}
+      <div>
+        <hr className="mt-16 mb-10 border-gray-200" />
+        <p className="text-sm text-center text-gray-600">
+          © Copyright 2024, All Rights Reserved by Team Status-Code
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Footer;
---
+++ Frontend/src/component/components/Navbar.jsx
@@ -0,0 +1,158 @@
+import { useState } from "react";
+import { auth, db } from "../../firebase/Firebase";
+import { signOut } from "firebase/auth";
+import { useNavigate } from "react-router";
+
+function Navbar() {
+  const navigate = useNavigate();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleSignOut = () => {
+    signOut(auth).then((val) => {
+      localStorage.removeItem("userId");
+      navigate("/");
+    });
+  };
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav className="bg-gray-800">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex-shrink-0">
+            <a href="/homepage" className="text-white font-bold text-xl">
+              भाषा Tuner
+            </a>
+          </div>
+          <div className="hidden md:block">
+            <div className="ml-4 flex items-baseline space-x-4">
+              <a
+                href="#"
+                onClick={() => navigate("/homepage")}
+                className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+              >
+                Home
+              </a>
+              <a
+                href="#"
+                onClick={() => navigate("/about-us")}
+                className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+              >
+                About Us
+              </a>
+              <a
+                href="#"
+                onClick={() => navigate("/contact-us")}
+                className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+              >
+                Contact Us
+              </a>
+              <a
+                href="#"
+                onClick={() => navigate("/profile")}
+                className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+              >
+                Profile Page
+              </a>
+              <a
+                href="#"
+                onClick={handleSignOut}
+                className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+              >
+                Logout
+              </a>
+            </div>
+          </div>
+          <div className="-mr-2 flex md:hidden">
+            <button
+              onClick={toggleMenu}
+              type="button"
+              className="bg-gray-900 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+              aria-controls="mobile-menu"
+              aria-expanded="false"
+            >
+              <span className="sr-only">Open main menu</span>
+              {isOpen ? (
+                <svg
+                  className="block h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  className="block h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              )}
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className={`${isOpen ? "block" : "hidden"} md:hidden`}>
+        <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+          <a
+            href="/homepage"
+            className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+          >
+            Home
+          </a>
+          <a
+            href="#"
+            onClick={() => navigate("/about-us")}
+            className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+          >
+            About Us
+          </a>
+          <a
+            href="#"
+            onClick={() => navigate("/contact-us")}
+            className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+          >
+            Contact Us
+          </a>
+          <a
+            href="#"
+            onClick={() => navigate("/profile")}
+            className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+          >
+            Profile
+          </a>
+          <a
+            href="#"
+            onClick={handleSignOut}
+            className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+          >
+            Logout
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
