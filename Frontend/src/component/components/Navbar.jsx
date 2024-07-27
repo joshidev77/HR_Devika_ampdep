4e867f7f30c1213723fcb9bef30d2f00c16cd7b6
Harsh Langaliya <langaliyaharsh2003@gmail.com>
2024-07-27 11:18:06 +0530

    Updated Woman

--- Frontend/src/component/components/Navbar.jsx
+++ Frontend/src/component/components/Navbar.jsx
@@ -23,7 +23,7 @@ function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex-shrink-0">
-            <a href="/homepage" className="text-white font-bold text-xl">
+            <a onClick={() => navigate("/homepage")} className="text-white font-bold text-xl">
               HR देविका
             </a>
           </div>
